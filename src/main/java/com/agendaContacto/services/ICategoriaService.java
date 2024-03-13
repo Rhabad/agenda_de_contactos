7bd828bec116c9416820e1f9868e6a1f65a109c3
Nicolas <nicolaspizarro.450@gmail.com>
2024-03-12 23:50:36 -0300

    interface service categoria

---
+++ src/main/java/com/agendaContacto/services/ICategoriaService.java
@@ -0,0 +1,13 @@
+package com.agendaContacto.services;
+
+import com.agendaContacto.models.dto.CategoriaDto;
+import com.agendaContacto.models.dto.CategoriaYContactoDto;
+
+import java.util.List;
+
+public interface ICategoriaService {
+    List<CategoriaYContactoDto> findAllCategoryAndContact(String nombreCategoria, Integer idCategoria);
+    CategoriaDto saveCategory(CategoriaDto categoriaDto);
+    void deleteCategory(Integer id);
+    CategoriaDto upgradeCategory(Integer id, CategoriaDto categoriaDto);
+}
