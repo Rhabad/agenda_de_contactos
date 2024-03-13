fb2101068350609c8d5cba8070d53d521a89fd08
Nicolas <nicolaspizarro.450@gmail.com>
2024-03-13 15:22:11 -0300

    agregando querys para traer categoria y contacto relacionado

--- src/main/java/com/agendaContacto/models/dao/CategoriaDao.java
+++ src/main/java/com/agendaContacto/models/dao/CategoriaDao.java
@@ -1,9 +1,15 @@
 package com.agendaContacto.models.dao;
 
 import com.agendaContacto.models.entities.Categoria;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoriaDao extends CrudRepository<Categoria, Integer> {
+    @Query(value = "select * from categoria where nombre_categoria = :nombreCategoria", nativeQuery = true)
+    Optional<Categoria> findByNombre(String nombreCategoria);
+
 }
--- src/main/java/com/agendaContacto/models/dao/ContactoDao.java
+++ src/main/java/com/agendaContacto/models/dao/ContactoDao.java
@@ -1,7 +1,13 @@
 package com.agendaContacto.models.dao;
 
 import com.agendaContacto.models.entities.Contacto;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ContactoDao extends CrudRepository<Contacto, Long> {
+
+    @Query(value = "select * from contacto where id_categoria = :idCategoria", nativeQuery = true)
+    List<Contacto> findAllByIdCategoria(Integer idCategoria);
 }
