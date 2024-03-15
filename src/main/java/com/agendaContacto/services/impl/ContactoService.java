7bd9f79b21fcfca64fd0c65014451c832bfd362d
Nicolas <nicolaspizarro.450@gmail.com>
2024-03-15 10:06:27 -0300

    listando todos los contactos del usuario

--- src/main/java/com/agendaContacto/services/impl/ContactoService.java
+++ src/main/java/com/agendaContacto/services/impl/ContactoService.java
@@ -1,16 +1,39 @@
 package com.agendaContacto.services.impl;
 
+import com.agendaContacto.models.dao.ContactoDao;
 import com.agendaContacto.models.dto.ContactoDto;
+import com.agendaContacto.models.entities.Contacto;
 import com.agendaContacto.services.IContactoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class ContactoService implements IContactoService {
+
+    @Autowired
+    private ContactoDao contactoDao;
+
+    /**
+     * listar todos los contactos de ese usuario
+     * @param idUsuario
+     * @return
+     */
     @Override
     public List<ContactoDto> findAllByIdUsuario(Long idUsuario) {
-        
-        return null;
+        List<Contacto> contactoList = contactoDao.findAllByIdUsuario(idUsuario);
+
+        List<ContactoDto> contactoDtoList = new ArrayList<>();
+        for (Contacto contacto: contactoList){
+            contactoDtoList.add(ContactoDto.builder()
+                            .telefono(contacto.getTelefono())
+                            .nombre(contacto.getNombre())
+                            .descripcion(contacto.getDescripcion())
+                            .email(contacto.getEmail())
+                    .build());
+        }
+        return contactoDtoList;
     }
 }
