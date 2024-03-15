1f082fa9544926a7047266073033716d33923a36
Nicolas <nicolaspizarro.450@gmail.com>
2024-03-15 00:10:27 -0300

    get all y que sea un list

--- src/main/java/com/agendaContacto/models/dao/CategoriaDao.java
+++ src/main/java/com/agendaContacto/models/dao/CategoriaDao.java
@@ -5,11 +5,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface CategoriaDao extends CrudRepository<Categoria, Integer> {
     @Query(value = "select * from categoria where nombre_categoria = :nombreCategoria and id_usuario = :idUsuario", nativeQuery = true)
     Optional<Categoria> findByNombreAndIdUsuario(String nombreCategoria, Long idUsuario);
+
+    @Query(value = "select * from categoria where id_usuario = :idUsuario", nativeQuery = true)
+    List<Categoria> findByIdUsuario(Long idUsuario);
 
 }
