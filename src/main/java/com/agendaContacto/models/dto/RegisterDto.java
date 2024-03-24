3675e9f95e18d216c64f7b8d63c29441b6a00e02
Nicolas <nicolaspizarro.450@gmail.com>
2024-03-24 18:48:37 -0300

    dto para registro

---
+++ src/main/java/com/agendaContacto/models/dto/RegisterDto.java
@@ -0,0 +1,9 @@
+package com.agendaContacto.models.dto;
+
+public class RegisterDto {
+    private String nombre;
+    private String username;
+    private String email;
+    private String password;
+
+}
